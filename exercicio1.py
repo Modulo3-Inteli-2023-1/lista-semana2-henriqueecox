f6a6f9457976c7cb76864c457475fbb0578d6f17
henriqueecox <123953851+henriqueecox@users.noreply.github.com>
2023-08-10 12:52:46 -0300

    Semana 2 - Henrique Cox
    Lista de exercícios Semana 2- Henrique Cox

--- exercicio1.py
+++ exercicio1.py
@@ -1,18 +1,14 @@
-#  Se achar necessario, faça import de outras bibliotecas
+def conta_palavras_unicas(frase):
+    # Divide a frase em palavras, ignora caracteres especiais e converte para minúsculas
+    palavras = [palavra.strip(".,!?").lower() for palavra in frase.split()]
+    # Usa um conjunto para remover palavras duplicadas e retorna o tamanho 
+    return len(set(palavras))
 
+# Exemplo de uso
+frase = "Esta é uma frase de exemplo. Esta frase contém palavras repetidas, mas queremos contar apenas as palavras únicas."
+quantidade_palavras_unicas = conta_palavras_unicas(frase)
+print("Quantidade de palavras únicas na frase:", quantidade_palavras_unicas)
 
-
-
-
-# Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
-
-# Teste a sua função aqui (caso ache necessário)
 
 
 
--- exercicio2.py
+++ exercicio2.py
@@ -1,18 +1,24 @@
-#  Se achar necessario, faça import de outras bibliotecas
+def is_anagram(str1, str2):
+    # Remover espaços e transformar em letras minúsculas
+    str1 = str1.replace(" ", "").lower()
+    str2 = str2.replace(" ", "").lower()
 
+    # Verificar se as strings têm o mesmo número de caracteres
+    if len(str1) != len(str2):
+        return False
 
+    # Converter as strings em listas de caracteres
+    list1 = list(str1)
+    list2 = list(str2)
 
+    # Verificar se as listas são iguais quando ordenadas
+    return sorted(list1) == sorted(list2)
 
-
-# Crie a função que será avaliada no exercício aqui
-
-
-
-
-
-
-
-# Teste a sua função aqui (caso ache necessário)
+# Exemplos de uso
+print(is_anagram("Amor", "Roma"))  # retornar True
+print(is_anagram("Pedra", "Padre"))  #  retornar True
+print(is_anagram("Perda", "Pedra"))  #  retornar True
+print(is_anagram("Hello", "World"))  #  retornar False
 
 
 
